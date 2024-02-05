befb7990c4bef54e42a6d6932a2d2cc76e40caf1
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2024-02-04 23:48:18 -0500

    Updated Problem 3

--- public_html/problem3.php
+++ public_html/problem3.php
@@ -15,9 +15,10 @@ function bePositive($arr)
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO use echo to output all of the values as positive (even if they were originally positive) and maintain the original datatype
     foreach ($arr as $key => $num) {
-        if ($num < 0)
+        if ($num < 0) {
             $arr[$key] = $num * -1;
-        settype($arr[$key], gettype($num));
+            settype($arr[$key], gettype($num));
+        }
     }
 
     foreach ($arr as $num) {
