f7a7bec2ac7cc948f89f4e19b1cbfa70969f47df
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2024-03-04 13:58:51 -0500

    Adding fixes to flash

--- partials/flash.php
+++ partials/flash.php
@@ -3,10 +3,14 @@
  populate the flash variable and then display at the proper timing*/
 ?>
 <div class="container" id="flash">
-    <?php $messages = getMessages(); ?>
+    <!-- can be blank or can have messages within it -->
+    <?php $messages = getMessages(); ?> 
     <?php if ($messages) : ?>
         <?php foreach ($messages as $msg) : ?>
+            <!-- integrated with bootstrap -->
             <div class="row justify-content-center">
+                <!-- color and text, color dictates the type of message via main colors -->
+                <!-- put at bottom, last thing that runs to make sure all relevant messages exist -->
                 <div class="alert alert-<?php se($msg, 'color', 'info'); ?>" role="alert"><?php se($msg, "text", ""); ?></div>
             </div>
         <?php endforeach; ?>
@@ -23,3 +27,14 @@
 
     moveMeUp(document.getElementById("flash"));
 </script>
+
+<!-- 
+    green - good, success
+    yellow orange - warning
+    red - danger
+    teal - info
+    light - off white
+    dark - off black
+    blue - primary  
+    grey - secondary
+-->
